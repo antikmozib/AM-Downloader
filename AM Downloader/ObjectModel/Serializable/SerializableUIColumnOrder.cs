0fae9ffd63697e0a74625406ff640536e6c8925a
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2023-06-07 15:48:45 +1000

    Serialization fixes, optimizations and cleanup

---
+++ AM Downloader/ObjectModel/Serializable/SerializableUIColumnOrder.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (C) 2020-2023 Antik Mozib. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+
+namespace AMDownloader.ObjectModel.Serializable
+{
+    [Serializable]
+    public class SerializableUIColumnOrder
+    {
+        public string ColumnName { get; set; }
+        public int ColumnIndex { get; set; }
+    }
+
+    [Serializable]
+    public class SerializableUIColumnOrderList
+    {
+        public List<SerializableUIColumnOrder> Objects;
+
+        public SerializableUIColumnOrderList()
+        {
+            Objects = new();
+        }
+    }
+}
