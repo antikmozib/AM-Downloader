e38d620ed173574ad8624fd7460d31c74855be3c
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2023-06-12 19:43:35 +1000

    Minor UI improvements and cleanup

---
+++ AM Downloader/Converters/ListViewerItemCountConverter.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (C) 2020-2023 Antik Mozib. All rights reserved.
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AMDownloader.Converters
+{
+    internal class ListViewerItemCountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+
+            int count = (int)value;
+
+            return count <= 1 ? $"{count} item" : $"{count} items";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
