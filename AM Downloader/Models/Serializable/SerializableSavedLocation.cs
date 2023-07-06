0b64c7360b278f3db4da8f20a210b9fea65c0bbf
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2023-07-06 18:20:06 +1000

    Rename SerializableSavedLocation

---
+++ AM Downloader/Models/Serializable/SerializableSavedLocation.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (C) 2020-2023 Antik Mozib. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+
+namespace AMDownloader.Models.Serializable
+{
+    [Serializable]
+    public class SerializableSavedLocation
+    {
+        public string Path { get; set; }
+    }
+
+    [Serializable]
+    public class SerializableSavedLocationList
+    {
+        public List<SerializableSavedLocation> Objects;
+
+        public SerializableSavedLocationList()
+        {
+            Objects = new List<SerializableSavedLocation>();
+        }
+    }
+}
