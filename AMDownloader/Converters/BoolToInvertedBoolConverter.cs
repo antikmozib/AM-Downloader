7d99ebbf0031d8c6fd3c3e825256eba74fe14ad3
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2024-11-16 22:18:30 +1100

    BoolToInvertedBoolConverter

---
+++ AMDownloader/Converters/BoolToInvertedBoolConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AMDownloader.Converters
+{
+    public class BoolToInvertedBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolVal)
+            {
+                return !boolVal;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
